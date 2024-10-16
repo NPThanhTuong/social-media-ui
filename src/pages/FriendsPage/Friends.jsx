6f235d0f48bc767a470c17d4b8543077efb9c311
Hoangit1506 <hoangit1506@gmail.com>
2024-10-16 21:16:02 +0700

    relationship

---
+++ src/pages/FriendsPage/Friends.jsx
@@ -0,0 +1,46 @@
+
+// import { Skeleton } from "@/components/ui/skeleton"
+
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function Friends({ type, list, handleConfirm }) {
+
+    const handleConfirmRequest = async (e, id) => {
+        e.preventDefault();
+        handleConfirm(id);
+    };
+
+    const handleDelete = async (e, id) => {
+        e.preventDefault();
+        handleConfirm(id);
+    };
+
+    const renderFriendCard = (friend) => {
+        const { id, name, avatar } = friend;
+        return (
+            <Link to={`/profile/${id}`} key={id} className="border p-4 rounded-md shadow-md w-60 flex flex-col items-center m-2 ">
+                <img src={avatar} alt={name} className="rounded-full w-20 h-20 object-cover mb-4" />
+                <span className="font-semibold">{name}</span>
+                {type === "requests" ? (
+                    <>
+                        <button className="bg-blue-500 text-white mt-2 px-4 py-1 rounded" onClick={(e) => handleConfirmRequest(e, id)}>Xác nhận</button>
+                        <button className="bg-gray-300 mt-2 px-4 py-1 rounded" onClick={(e) => handleDelete(e, id)}>Xóa</button>
+                    </>
+                ) : type === "requestsSent" ? (
+                    <button className="bg-gray-300 mt-2 px-4 py-1 rounded ">Xóa</button>
+                ) : null}
+            </Link>
+        );
+    };
+
+    if (!list) {
+        return;
+    }
+
+    return (
+        <div className="flex flex-wrap justify-start">
+            {list.map(renderFriendCard)}
+        </div>
+    );
+}
